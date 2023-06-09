866f9cfdd756cfd4ae4bda8b8f426725c00d193c
Debojyoti <debojyoti.chatterjee9@gmail.com>
2023-06-09 20:38:58 +0530

    fix: updating the primary key to uuidv4

--- src/modules/company/company.schema.ts
+++ src/modules/company/company.schema.ts
@@ -1,10 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import {v4 as uuidv4} from 'uuid';
 
 export type CompanyDocument = HydratedDocument<Company>;
 
 @Schema()
 export class Company {
+  @Prop({primary: true, default: uuidv4()})
+  _id: String
   @Prop({ required: true })
   name: String;
   @Prop()
