d99bca6ba5297de98f230ccaf9f76d5aed41b8ff
Debojyoti <debojyoti.chatterjee9@gmail.com>
2023-06-09 20:05:07 +0530

    feat: initial company model

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyController } from './company.controller';
+import { Company, CompanySchema } from './company.schema';
 
 @Module({
+  imports:[MongooseModule.forFeature([{name: Company.name, schema: CompanySchema}])],
   controllers: [CompanyController]
 })
 export class CompanyModule {}
---
+++ src/modules/company/company.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type CompanyDocument = HydratedDocument<Company>;
+
+@Schema()
+export class Company {
+  @Prop({required: true})
+  name: String;
+  @Prop()
+  ceo: String;
+  @Prop()
+  address: String;
+  @Prop()
+  inception: Date;
+}
+
+export const CompanySchema = SchemaFactory.createForClass(Company);
