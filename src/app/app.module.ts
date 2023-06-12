a510167fcd4907a72b7a1b3b00b3bdfb325a671d
Debojyoti <debojyoti.chatterjee9@gmail.com>
2023-06-12 23:48:17 +0530

    feat: adding boot files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { AuthorizationModule } from 'src/modules/authorization/authorization.module';
 import { CompanyModule } from '../modules/company/company.module';
 import { TeamModule } from 'src/modules/team/team.module';
+import * as dotenv from 'dotenv';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/techwondoe_round_1'),
+  imports: [MongooseModule.forRoot(process.env.MONGODB_DOCKER_URI),
   AuthorizationModule, CompanyModule, TeamModule],
   controllers: [AppController],
   providers: [AppService],
